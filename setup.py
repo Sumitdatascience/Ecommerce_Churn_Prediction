8f5938dd0413aa3b1a32eb4b6d73cc5430d2c4bc
Sumit Kumar Sourabh <sumitsourabh1993@gmail.com>
2024-09-28 01:04:46 +0530

    Add cookiecutter templete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='E commerce churn prediction',
+    author='CS_Soft',
+    license='',
+)
---
+++ src/data/data_ingestion.py
@@ -0,0 +1,31 @@
+
+import pandas as pd
+import numpy as np
+import os
+
+# Function to load the data
+def load_data(data_path: str) -> pd.DataFrame:
+    df = pd.read_csv(data_path, on_bad_lines='skip')
+    df = df.drop(columns='Unnamed: 20')
+    df = df.drop(columns='CustomerID')
+    df = df.drop(columns='CouponUsed')
+    
+    return df
+
+# Save the data locally
+def save_data(data_path: str, df: pd.DataFrame) -> None:
+    os.makedirs(data_path, exist_ok=True)  
+    df.to_csv(os.path.join(data_path, "df.csv"), index=False)
+    
+
+# Main function to load and save data
+def main() -> None:
+    df = load_data(r"C:\Users\Sumit\Downloads\customer_data_model_refined (4) (2).csv")
+
+    data_path = os.path.join("data", "raw")
+    
+    save_data(data_path, df)
+
+# Correct the __main__ check
+if __name__ == "__main__":
+    main()
